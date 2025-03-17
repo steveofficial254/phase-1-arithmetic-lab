55f6d8853d7d892f1ccd2753475621a8f83e3e61
stephen mburu <stephenmburu8509@gmail.com>
2025-03-17 13:41:25 +0300

    adds arithmetic operation

--- index.js
+++ index.js
@@ -1,2 +1,17 @@
 
 // Write your code here
+let num1 = 31;
+let num2 = 2;
+let multiply = num1 * num2;
+console.log(multiply);
+
+let random = Math.floor(Math.random() * 10) + 1;
+console.log(random);
+
+let num3 = 10;
+let num4 = 6;
+let mod = num3 % num4;
+console.log(mod);
+
+let max= Math.max(11, 12, 13, 14, 15, 16, 17, 18, 19, 20);
+console.log(max)
